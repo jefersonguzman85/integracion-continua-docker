901833cab48632ff354252b45b670beb0f3c7f1b
Ancizar Rubio <ancizar.rubio@gmail.com>
2020-11-20 19:23:26 -0500

    update

--- registration/registration/settings.py
+++ registration/registration/settings.py
@@ -130,6 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 STATIC_ROOT = 'code/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
